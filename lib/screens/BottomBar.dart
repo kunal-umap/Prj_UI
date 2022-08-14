18c5bfd6db67e8543e56df322b68ef6cb30099fc
Kunal Umap <kunalgumap@gmail.com>
2022-08-14 17:01:21 +0530

    bottom bar added

--- lib/screens/BottomBar.dart
+++ lib/screens/BottomBar.dart
@@ -1,3 +1,4 @@
+import 'package:app_ui/screens/homePage.dart';
 import 'package:flutter/material.dart';
 
 class BottomBar extends StatefulWidget {
@@ -11,10 +12,44 @@ class _BottomBarState extends State<BottomBar> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HouseZet"),
-      ),
-      body: Center(child: Text("Body")),
+      body: homePage(),
+      bottomNavigationBar:  BottomNavigationBar(
+          currentIndex:0,
+
+          selectedItemColor: Colors.deepPurple,
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home_rounded,
+              ),
+              label: "Home",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.lock,
+              ),
+              label: "Lock",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.document_scanner,
+              ),
+              label: "Scan",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.message_outlined,
+              ),
+              label: "Message",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(
+                Icons.add_alert_rounded,
+              ),
+              label: "Alert",
+            ),
+          ]),
     );
   }
 }
---
+++ lib/screens/homePage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  State<homePage> createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Main Body"),
+      ),
+    );
+  }
+}
