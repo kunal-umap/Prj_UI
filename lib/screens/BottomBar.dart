5fbb31405988cceeeafbaf0b1f2dbd24511a612f
Kunal Umap <kunalgumap@gmail.com>
2022-08-14 15:34:41 +0530

    first commit

---
+++ lib/screens/BottomBar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class BottomBar extends StatefulWidget {
+  const BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HouseZet"),
+      ),
+      body: Center(child: Text("Body")),
+    );
+  }
+}
